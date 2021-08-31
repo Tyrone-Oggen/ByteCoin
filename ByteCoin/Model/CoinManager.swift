9b240320284df05179f17a7313924528a265dabf
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-08-31 18:56:14 +0200

    Coin manager methods added: Protocol for VC calls, JSON Parse method and the urlString interpolation

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,56 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateCoin(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "30F56457-2A7C-4C1C-9682-4AC99EBC7B93"
+    
+    var delegate: CoinManagerDelegate?
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
-    
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let convertedCoin = parseJSON(safeData) {
+                        let rateString = String(format: "%.2f", convertedCoin)
+                        
+                        delegate?.didUpdateCoin(price: rateString, currency: currency)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let rate = decodedData.rate
+            
+            return rate
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
 }
