a655b6b356d8811060e79e3f505ec942e76678f0
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-08-31 18:50:38 +0200

    CoinData struct for JSON parsing added

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Tyrone Oggen on 2021/08/31.
+//  Copyright © 2021 Tyrone Oggen. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
