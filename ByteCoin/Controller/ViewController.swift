c25768edc9556dc1172a386e856abf22facbbadc
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-08-31 18:57:15 +0200

    VC added as delegate; ViewPicker delegate methods added and CoinManagerDelegate updating UI

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,22 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource {
-
+    
     @IBOutlet weak var bitcoinConversionLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
+    var coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        <#code#>
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        
+        coinManager.delegate = self
     }
 }
 
+//MARK: - PickerView content management
+extension ViewController: UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        coinManager.currencyArray.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
+    
+}
+
+extension ViewController: CoinManagerDelegate {
+    func didUpdateCoin(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinConversionLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+    
+}
